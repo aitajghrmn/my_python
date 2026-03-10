106c772a0525585f23adeb400dee5b1ce566d326
aitajghrmn <businessqehremanqizi@gmail.com>
2026-03-10 12:03:25 +0400

    multiple

---
+++ while_loop/multiplationtable.py
@@ -0,0 +1,14 @@
+"""num=int(input("Enter the number: "))
+hasil=1
+i=1
+while i<num:
+    hasil=hasil*i
+    i=i+1
+    print(hasil) """
+
+num = int(input("Enter the number: "))
+
+i = 1
+while i <= 10:
+    print(num, "x", i, "=", num * i)
+    i = i + 1
